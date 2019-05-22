6356925f5839824a0728e7bc4b36d683d04a73e2
ArekChr <arkadiusz.chrabaszczewski@deva.as>
2019-05-22 08:52:04 +0200

    use react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
 import { background } from './consts/colors'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -20,19 +20,22 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-library.add(faUserCircle)
-library.add(faPlane)
-library.add(faCreditCard)
-library.add(faLifeRing)
-library.add(faPhone)
-library.add(faSignOutAlt)
-library.add(faQuestionCircle)
-library.add(faInfoCircle)
-library.add(faHome)
-library.add(faIdCard)
-library.add(faShoppingCart)
-library.add(faTimes)
-library.add(faBars)
+library.add(
+  faUserCircle,
+  faPlane,
+  faCreditCard,
+  faLifeRing,
+  faPhone,
+  faSignOutAlt,
+  faQuestionCircle,
+  faInfoCircle,
+  faHome,
+  faIdCard,
+  faShoppingCart,
+  faTimes,
+  faBars
+)
+
 
 const logo = `${process.env.PUBLIC_URL}/awaymoFullWhite.svg`
 const avatar = `${process.env.PUBLIC_URL}/avatar.png`
@@ -206,94 +209,84 @@ const defaultStyle = {
   transition: `right ${duration}ms ease-in-out`,
 }
 
-class App extends React.Component {
+const App = () => {
 
-  state = {
-    showMenu: true,
-    transitionStyles: {
-      entering: { right: 0 },
-      entered: { right: 0 },
-      exiting: { right: window.innerWidth },
-      exited: { right: window.innerWidth }
+  const [transitionStyles, setTransitionStyles] = useState({
+    entering: { right: 0 },
+    entered: { right: 0 },
+    exiting: { right: window.innerWidth },
+    exited: { right: window.innerWidth }
+  })
+
+  const [menu, setMenu] = useState(true)
+
+  useEffect(() => {
+    const resize = () => {
+      setTransitionStyles({
+        entering: { right: 0 },
+        entered: { right: 0 },
+        exiting: { right: window.innerWidth },
+        exited: { right: window.innerWidth }
+        }
+      )
     }
-  }
 
-  componentDidMount() {
-    window.addEventListener("resize", () => this.resize());
-    this.resize();
-    this.setState({
-      transitionStyles: {
-        entering: { right: 0 },
-        entered: { right: 0 },
-        exiting: { right: window.innerWidth },
-        exited: { right: window.innerWidth }
-      }
-    })
-  }
+    resize();
+    window.addEventListener("resize", resize)
 
-  resize() {
-    this.setState({
-      transitionStyles: {
-        entering: { right: 0 },
-        entered: { right: 0 },
-        exiting: { right: window.innerWidth },
-        exited: { right: window.innerWidth }
-      }
-    })
-  }
+    return () => window.removeEventListener("resize", resize) 
+  })
 
-  render() {
-    return (
-      <Transition in={this.state.showMenu} timeout={duration}>
-        {state => (
-          <Container style={{
-            ...defaultStyle,
-            ...this.state.transitionStyles[state]
-          }}>
-            <Open onClick={() => this.setState({ showMenu: !this.state.showMenu })}>
-              <Icon icon="bars" color="white" size="lg"/>
-            </Open>
-            <Logo>
-              <img src={logo} alt="logo" />
-              <CloseIcon onClick={() => this.setState({ showMenu: !this.state.showMenu })} icon="times" color="white" size="1x"/>
-            </Logo>
-            <Profile>
-              <Ring>
-                <Avatar src={avatar} alt="avatar"/>
-              </Ring>
-              <Details>
-                <Text>Dominik <Text mobile>Biel</Text></Text>
-                <BalanceDetails>
-                  <Text>Avaiable <Text mobile>Balance</Text></Text>
-                  <Amount>£1,500.00</Amount>
-                </BalanceDetails>
-              </Details>
-            </Profile>
-            <div>
-              <MenuItem><Icon icon="user-circle" color="white" size="1x"/>Profile</MenuItem>
-              <MenuItem><Icon icon="credit-card" color="white" size="1x"/>My Payments</MenuItem>
-              <MenuItem><Icon icon="shopping-cart" color="white" size="1x"/>My Bookings</MenuItem>
-              <MenuItem><Icon icon="home" color="white" size="1x"/>Home</MenuItem>
-              <MenuItem><Icon icon="plane" color="white" size="1x" transform={{ rotate: -45 }}/>Flights</MenuItem>
-              <MenuItem><Icon icon="question-circle" color="white" size="1x"/>About Us</MenuItem>
-              <MenuItem><Icon icon="info-circle" color="white" size="1x"/>FAQ</MenuItem>
-              <MenuItem><Icon icon="life-ring" color="white" size="1x"/>Support</MenuItem>
-              <MenuItem><Icon icon="phone" color="white" size="1x"/>Contact Us</MenuItem>
-              <MenuItem><Icon icon="sign-out-alt" color="white" size="1x"/>Log Out</MenuItem>
-              <MenuItem><Icon icon="id-card" color="white" size="1x"/>Resume Application</MenuItem>
-            </div>
-            <Footer>
-              <Text>We're here to help</Text>
-              <Mobile column>
-                <Text>+44 (0) 20 8050 3459 </Text>
-                <Text>support@awaymo.com</Text>
-              </Mobile>
-            </Footer>
-          </Container>
-        )}
-      </Transition>
-    );
-  }
+  return (
+    <Transition in={menu} timeout={duration}>
+      {state => (
+        <Container style={{
+          ...defaultStyle,
+          ...transitionStyles[state]
+        }}>
+          <Open onClick={() => setMenu(!menu)}>
+            <Icon icon="bars" color="white" size="lg"/>
+          </Open>
+          <Logo>
+            <img src={logo} alt="logo" />
+            <CloseIcon onClick={() => setMenu(!menu)} icon="times" color="white" size="1x"/>
+          </Logo>
+          <Profile>
+            <Ring>
+              <Avatar src={avatar} alt="avatar"/>
+            </Ring>
+            <Details>
+              <Text>Dominik <Text mobile>Biel</Text></Text>
+              <BalanceDetails>
+                <Text>Avaiable <Text mobile>Balance</Text></Text>
+                <Amount>£1,500.00</Amount>
+              </BalanceDetails>
+            </Details>
+          </Profile>
+          <div>
+            <MenuItem><Icon icon="user-circle" color="white" size="1x"/>Profile</MenuItem>
+            <MenuItem><Icon icon="credit-card" color="white" size="1x"/>My Payments</MenuItem>
+            <MenuItem><Icon icon="shopping-cart" color="white" size="1x"/>My Bookings</MenuItem>
+            <MenuItem><Icon icon="home" color="white" size="1x"/>Home</MenuItem>
+            <MenuItem><Icon icon="plane" color="white" size="1x" transform={{ rotate: -45 }}/>Flights</MenuItem>
+            <MenuItem><Icon icon="question-circle" color="white" size="1x"/>About Us</MenuItem>
+            <MenuItem><Icon icon="info-circle" color="white" size="1x"/>FAQ</MenuItem>
+            <MenuItem><Icon icon="life-ring" color="white" size="1x"/>Support</MenuItem>
+            <MenuItem><Icon icon="phone" color="white" size="1x"/>Contact Us</MenuItem>
+            <MenuItem><Icon icon="sign-out-alt" color="white" size="1x"/>Log Out</MenuItem>
+            <MenuItem><Icon icon="id-card" color="white" size="1x"/>Resume Application</MenuItem>
+          </div>
+          <Footer>
+            <Text>We're here to help</Text>
+            <Mobile column>
+              <Text>+44 (0) 20 8050 3459 </Text>
+              <Text>support@awaymo.com</Text>
+            </Mobile>
+          </Footer>
+        </Container>
+      )}
+    </Transition>
+  );
 }
 
 export default App;
